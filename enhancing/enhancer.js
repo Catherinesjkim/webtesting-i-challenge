06341c67e87ada692b1f1dac30bbebe461c94036
Catherine Kim <catherine@ipwe.com>
2020-06-09 12:36:47 -0700

    MVP Met Because: - Wrote unit tests and production code to satisfy the MVP - Automated testing - Used Jest as a test runner - Wrote unit tests for JS functions - Used the TDD workflow
    This commit:
    - Wrote unit tests and the implementation code for a module that helps players see the results of a success or failure when trying to enhnce their in-game items.

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,66 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  // if enhancement is less tahn 20 you will get a +1
+  if (item.enhancement < 20) {
+    const successItem = {
+      name: item.name, 
+      durability: item.durability, 
+      enhancement: item.enhancement + 1,
+    }
+    return successItem
+    // if enhancement is === 20 you will get no change on the enhancement
+  } else if (item.enhancement === 20) {
+    const successItem = {
+      name: item.name, 
+      durability: item.durability,
+      enhancement: item.enhancement,
+    }
+    return successItem
+    // if enhancement reaches a NUMBER > 20 display error
+  } else if (item.enhancement > 20 || item.durability > 100) {
+    const error = "Please don't try to add a bigger number than 20."
+    return error
+  }
 }
 
 function fail(item) {
-  return { ...item };
-}
+  if (item.enhancement < 15) {
+    const failItem = {
+      name: item.name,
+      durability: item.durability - 5,
+      enhancement: item. enhancement,
+      }
+      return failItem
+    } else if (item.enhancement >= 15) {
+      const failItem = {
+        name: item.name,
+        durability: item.durability - 10,
+        enhancement: item.enhancement - 1,
+      }
+      return failItem
+    } else if (item.enhancement > 16) {
+      const failItem = {
+        name: item.name, 
+        durability: item.durability,
+        enhancement: item.enhancement - 1,
+      }
+      return failItem
+    }
+  }
 
+// when an item is reparied the only thing that changes is the durability.
 function repair(item) {
-  return { ...item };
+  if (item.durability === 0 || item.durability < 101) {
+    const repairedItem = {
+      name: item.name,
+      durability: 100,
+      enhancement: item.enhancement, 
+    }
+    return repairedItem
+  }
 }
 
 function get(item) {
-  return { ...item };
+  return { ...item }
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,138 @@
-const enhancer = require('./enhancer.js');
+const { repair, succeed, fail } = require('./enhancer.js');
 // test away!
+
+// Everything related to repair()...
+// passes = completed && Fail = Completed
+// -- TODO => If durability is set to be greater than 100, it should not pass.
+describe("enhancer.js", () => {
+  describe("repair()", () => {
+    it("should make durability from Number to 100...", () => {
+      const items = {
+        name: "Catherine",
+        durability: 43, 
+        enhancement: 20, 
+      }
+
+      const expects = {
+        name: "Catherine",
+        durability: 100, 
+        enhancement: 20,
+      }
+
+      const itemRepaired = repair(items)
+
+      expect(itemRepaired).toEqual(expects)
+    })
+  })
+})
+
+// Everything related to success()...
+// You are able to check all conditions for succeed() function
+// passes = completed && Fail = Completed
+describe("enhancer.js", () => {
+  describe("success()", () => {
+    it("should make enhancement current + 1", () => {
+      const items = {
+        name: "Catherine",
+        durability: 12,
+        enhancement: 18,
+      }
+
+      const expects = {
+        name: "Catherine",
+        durability: 12, 
+        enhancement: 19, 
+      }
+
+      const itemEnhanced = succeed(items)
+
+      expect(itemEnhanced).toEqual(expects)
+    })
+  })
+})
+
+// passes = completed && Fail = Completed
+describe("enhancer.js", () => {
+  describe("success()", () => {
+    it("should make enhancement state = unchanged", () => {
+      const items = {
+        name: "Catherine",
+        durability: 12, 
+        enhancement: 20,
+      }
+
+      const expects = {
+        name: "Catherine",
+        durability: 12, 
+        enhancement: 20,
+      }
+
+      const itemEnhanced = succeed(items)
+
+      expect(itemEnhanced).toEqual(expects)
+    })
+  })
+})
+
+// Everything related to fail()...
+// passes = completed && Fail = Completed
+describe("enhancer.js", () => {
+  describe("fail()", () => {
+    it("if enhancement of (item) < 15 = durability - 5", () => {
+      const items = {
+        name: "Catherine",
+        durability: 100,
+        enhancement: 13,
+      }
+      const expects = {
+        name: "Catherine",
+        durability: 95,
+        enhancement: 13,
+      }
+      const itemFail = fail(items)
+
+      expect(itemFail).toEqual(expects)
+    })
+  })
+})
+
+// passes = completed && Fail = Completed
+describe("enhancer.js", () => {
+  describe("fail()", () => {
+    it("if enhancement of (item) >= 15 = durability - 10", () => {
+      const items = {
+        name: "Catherine",
+        durability: 100,
+        enhancement: 15,
+      }
+      const expects = {
+        name: "Catherine",
+        durability: 90,
+        enhancement: 14,
+      }
+      const itemFail = fail(items)
+
+      expect(itemFail).toEqual(expects)
+    })
+  })
+})
+
+describe("enhancer.js", () => {
+  describe("fail()", () => {
+    it("if enhancement of (item) > 16 = enhancement -1", () => {
+      const item = {
+        name: "Catherine",
+        durability: 55,
+        enhancement: 17, 
+      }
+      const expects = {
+        name: "Catherine",
+        durability: 45,
+        enhancement: 16, 
+      }
+      const itemFail = fail(item)
+
+      expect(itemFail).toEqual(expects)
+    })
+  })
+})
